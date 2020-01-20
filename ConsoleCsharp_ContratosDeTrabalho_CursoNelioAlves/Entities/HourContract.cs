41d86cc07103404401217cbe0bb84cec64bef939
Luis Everton <luis.everton@outlook.com>
2020-01-20 14:44:59 -0300

    Criando classe HourContract

---
+++ ConsoleCsharp_ContratosDeTrabalho_CursoNelioAlves/Entities/HourContract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCsharp_ContratosDeTrabalho_CursoNelioAlves.Entities
+{
+    class HourContract
+    {
+        //propriedades
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        //contrutor padrão
+        public HourContract()
+        {
+        }
+
+        //contrutor com argumentos/parametros
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+    }
+}
